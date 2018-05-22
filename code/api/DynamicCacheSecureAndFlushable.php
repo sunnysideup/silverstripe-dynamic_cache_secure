4b4fe2597bf6d641f5d806c35dbca45e28a88094
Sunny Side Up <github@sunnysideup.co.nz>
2018-05-22 16:14:02 +1200

    PATCH: PSR-2

--- code/api/DynamicCacheSecureAndFlushable.php
+++ code/api/DynamicCacheSecureAndFlushable.php
@@ -1,8 +1,9 @@
 <?php
 
-class DynamicCacheSecureAndFlushable extends DynamicCacheExtension implements flushable {
-
-    public function updateEnabled(&$enabled) {
+class DynamicCacheSecureAndFlushable extends DynamicCacheExtension implements flushable
+{
+    public function updateEnabled(&$enabled)
+    {
         
         // Disable caching for this request if a user is logged in
         if (Member::currentUserID()) {
@@ -17,13 +18,14 @@ class DynamicCacheSecureAndFlushable extends DynamicCacheExtension implements fl
         // Disable caching if the request is in dev mode
         else {
             $session = Session::get_all();
-            if($session && count($session)) {
+            if ($session && count($session)) {
                 $enabled = false;
             }
         }
     }
 
-    public static function flush() {
+    public static function flush()
+    {
         DynamicCache::inst()->clear();
     }
 }
